19adf52c6b209eeb55b2717d8cff0853de91fe71
Anna Wester <annawester73@gmail.com>
2026-04-10 23:25:30 +0200

    Commit with fix in book.py

--- src/book.py
+++ src/book.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author, borrowed=False):
+    def __init__(self, title, author, borrowed):
         self.title = title
         self.author = author
         self.borrowed = borrowed
